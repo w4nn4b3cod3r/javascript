232f0694a20f1ff7635c51af48980d34ca71ce2f
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2020-07-19 20:00:34 +0600

    learing javascript

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 let timer = document.getElementById("timer");
 let startBtn = document.getElementById("startBtn");
 let resetBtn = document.getElementById("resetBtn");
-
+let myText = document.getElementById("myText");
 let  watch = new Stopwatch(timer);
+
+isDefault = myText
 
 function start() {
     startBtn.textContent = 'Stop';
@@ -20,3 +22,14 @@ startBtn.addEventListener('click', function() {
 resetBtn.addEventListener('click', function() {
     watch.reset();
 });
+
+// change the color of the header text 
+myText.addEventListener('mouseover', function() {
+    document.getElementById('myText').style.color = '#ff6666';
+});
+
+myText.addEventListener('mouseleave', function() {
+    document.getElementById('myText').style.color = 'black';
+});
+
+// ends here 
